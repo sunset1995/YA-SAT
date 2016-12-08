f80a3701742979815a2b75b054c46f3f6e1368af
Sunset <s2821d3721@gmail.com>
2016-12-08 20:59:58 +0800

    Update test script

--- test.py
+++ test.py
@@ -7,37 +7,29 @@ if len(sys.argv) != 2:
     print('python3 test.py testbenchDir')
     exit(0)
 
-TESTNUM = 0
 SAT = 0
 UNSAT = 0
 ERROR = 0
-MAXTIME = 0
-TOTALTIME = 0
-MAXBACKTRACKNUM = 0
-TOTALBACKTRACKNUM = 0
-MAXDEPTH = 0
-TOTALDEPTH = 0
-MAXLEANRTCLS = 0
-TOTALLEANRTCLS = 0
-MAXLEANRTASS = 0
-TOTALLEANRTASS = 0
-MAXJUMPBACK = 0
-TOTALJUMPBACK = 0
-MAXLEANRTSZ = 0
-TOTALLEANRTSZ = 0
+
+title = []
+statistic = []
 
 for dirname, dirnames, filenames in os.walk(sys.argv[1]):
     for filename in filenames:
+        
+        # Run only .cnf files
         if filename.find('.cnf') == -1:
             continue
-        TESTNUM = TESTNUM + 1
-        print('=============================')
-        print(os.path.join(dirname, filename))
-        proc = subprocess.Popen(['./yasat', '-stdout', '-statistic', os.path.join(dirname, filename)],
+
+        print(os.path.join(dirname, filename).center(70, "-"))
+        proc = subprocess.Popen([
+            './yasat', '-stdout', '-statistic',
+            os.path.join(dirname, filename)],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,)
         proc.wait()
         result = str(proc.stdout.read())
+
         if proc.returncode != 0:
             ERROR = ERROR + 1
             continue
@@ -47,56 +39,29 @@ for dirname, dirnames, filenames in os.walk(sys.argv[1]):
         else:
             print('SAT')
             SAT = SAT + 1
-        statistic = str(proc.stderr.read()).split('\\n')
-        nowTime = float(re.findall('[-+]?\d*\.\d+|[-+]?\d+', statistic[1])[0])
-        nowBacktrackNum = [int(v) for v in statistic[2].split() if v.isdigit()][0]
-        nowDepth = [int(v) for v in statistic[3].split() if v.isdigit()][0]
-        nowLearnCls = [int(v) for v in statistic[4].split() if v.isdigit()][0]
-        nowLearnAss = [int(v) for v in statistic[5].split() if v.isdigit()][0]
-        nowJumpBack = [int(v) for v in statistic[6].split() if v.isdigit()][0]
-        nowLearntSz = [int(v) for v in statistic[7].split() if v.isdigit()][0]
-        print('Time for SAT solver', nowTime, 'sec')
-        print('Backtrack Num', nowBacktrackNum)
-        print('Max depth', nowDepth)
-        print('Learnt clause', nowLearnCls)
-        print('Learnt assignment', nowLearnAss)
-        print('Max jump back', nowJumpBack)
-        print('Max learnt sz', nowLearntSz)
-        MAXTIME = max(MAXTIME, nowTime)
-        TOTALTIME = TOTALTIME + nowTime
-        MAXBACKTRACKNUM = max(MAXBACKTRACKNUM, nowBacktrackNum)
-        TOTALBACKTRACKNUM = TOTALBACKTRACKNUM + nowBacktrackNum
-        MAXDEPTH = max(MAXDEPTH, nowDepth)
-        TOTALDEPTH = TOTALDEPTH + nowDepth
-        MAXLEANRTCLS = max(MAXLEANRTCLS, nowLearnCls)
-        TOTALLEANRTCLS = TOTALLEANRTCLS + nowLearnCls
-        MAXLEANRTASS = max(MAXLEANRTASS, nowLearnAss)
-        TOTALLEANRTASS = TOTALLEANRTASS + nowLearnAss
-        MAXJUMPBACK = max(MAXJUMPBACK, nowJumpBack)
-        TOTALJUMPBACK = TOTALJUMPBACK + nowJumpBack
-        MAXLEANRTSZ = max(MAXLEANRTSZ, nowLearntSz)
-        TOTALLEANRTSZ = TOTALLEANRTSZ + nowLearntSz
 
-print('=============================')
+        # Read statistic
+        nowStat = str(proc.stderr.read()).split('\\n')
+
+        # Check init
+        if len(statistic)==0:
+            title = [''] * len(nowStat)
+            statistic = [[0.0, 0.0] for _ in range(len(nowStat))]
+            for i in range(1, len(nowStat)-2):
+                title[i] = nowStat[i].split(':')[0]
+
+        # Parse statistic
+        for i in range(1, len(nowStat)-2):
+            print(nowStat[i])
+            nowVal = float(re.findall('[-+]?\d*\.\d+|[-+]?\d+', nowStat[i])[0])
+            statistic[i][0] += nowVal
+            statistic[i][1] = max(nowVal, statistic[i][1])
+
+        print("")
+
+
+print(" statistic ".center(70, "-"))
 print('SAT', SAT, '/ UNSAT', UNSAT, '/ ERROR', ERROR)
-print('\nTime for SAT Solver')
-print('   avg %.3f sec' % (TOTALTIME / (SAT + UNSAT)))
-print('   max %.3f sec' % (MAXTIME))
-print('\nBacktrack Num')
-print('   avg', int(TOTALBACKTRACKNUM/(SAT + UNSAT)))
-print('   max', MAXBACKTRACKNUM)
-print('\nMax Depth')
-print('   avg', int(TOTALDEPTH/(SAT + UNSAT)))
-print('   max', MAXDEPTH)
-print('\nLearnt clause')
-print('   avg', TOTALLEANRTCLS/(SAT + UNSAT))
-print('   max', MAXLEANRTCLS)
-print('\nLearnt assignment')
-print('   avg', TOTALLEANRTASS/(SAT + UNSAT))
-print('   max', MAXLEANRTASS)
-print('\nMax jump back')
-print('   avg', int(TOTALJUMPBACK/(SAT + UNSAT)))
-print('   max', MAXJUMPBACK)
-print('\nMax learnt sz')
-print('   avg', int(TOTALLEANRTSZ/(SAT + UNSAT)))
-print('   max', MAXLEANRTSZ)
+for i in range(1, len(statistic)-2):
+    print("%s : %10.2f / %10.2f" % (title[i], statistic[i][0]/(SAT + UNSAT), statistic[i][1]))
+print("".center(70, "="))
--- testunsat.py
+++ testunsat.py
@@ -3,41 +3,36 @@ import os
 import sys
 import subprocess
 
-print('======== UNSAT ========')
+print(" ALL UNSAT ".center(50, "="))
 basename = 'benchmarks/UNSAT'
 _, dirnames, _ = tuple(os.walk(basename))[0]
 for scale in dirnames:
-    print("\n======== %s ========" % scale)
+    print("")
+    print(scale.center(50, "-"))
     _, _, problems = tuple(os.walk('%s/%s' % (basename, scale)))[0]
 
     SAT = 0
     UNSAT = 0
     ERROR = 0
-    MAXTIME = 0
-    TOTALTIME = 0
-    MAXBACKTRACKNUM = 0
-    TOTALBACKTRACKNUM = 0
-    MAXDEPTH = 0
-    TOTALDEPTH = 0
-    MAXLEANRTCLS = 0
-    TOTALLEANRTCLS = 0
-    MAXLEANRTASS = 0
-    TOTALLEANRTASS = 0
-    MAXJUMPBACK = 0
-    TOTALJUMPBACK = 0
-    MAXLEANRTSZ = 0
-    TOTALLEANRTSZ = 0
+
+    title = []
+    statistic = []
 
     for problem in problems:
+
+        # Run only .cnf files
         if problem.find('.cnf') == -1:
             continue
         proc = subprocess.Popen([
-            './yasat', '-stdout', '-statistic', 
+            './yasat', '-stdout', '-statistic',
             "%s/%s/%s" % (basename, scale, problem)],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,)
         proc.wait()
         result = str(proc.stdout.read())
+
+
+        # Read SAT/UNSAT/ERROR
         if proc.returncode != 0:
             ERROR = ERROR + 1
             continue
@@ -45,48 +40,26 @@ for scale in dirnames:
             UNSAT = UNSAT + 1
         else:
             SAT = SAT + 1
-        statistic = str(proc.stderr.read()).split('\\n')
-        nowTime = float(re.findall('[-+]?\d*\.\d+|[-+]?\d+', statistic[1])[0])
-        nowBacktrackNum = [int(v) for v in statistic[2].split() if v.isdigit()][0]
-        nowDepth = [int(v) for v in statistic[3].split() if v.isdigit()][0]
-        nowLearnCls = [int(v) for v in statistic[4].split() if v.isdigit()][0]
-        nowLearnAss = [int(v) for v in statistic[5].split() if v.isdigit()][0]
-        nowJumpBack = [int(v) for v in statistic[6].split() if v.isdigit()][0]
-        nowLearntSz = [int(v) for v in statistic[7].split() if v.isdigit()][0]
-        MAXTIME = max(MAXTIME, nowTime)
-        TOTALTIME = TOTALTIME + nowTime
-        MAXBACKTRACKNUM = max(MAXBACKTRACKNUM, nowBacktrackNum)
-        TOTALBACKTRACKNUM = TOTALBACKTRACKNUM + nowBacktrackNum
-        MAXDEPTH = max(MAXDEPTH, nowDepth)
-        TOTALDEPTH = TOTALDEPTH + nowDepth
-        MAXLEANRTCLS = max(MAXLEANRTCLS, nowLearnCls)
-        TOTALLEANRTCLS = TOTALLEANRTCLS + nowLearnCls
-        MAXLEANRTASS = max(MAXLEANRTASS, nowLearnAss)
-        TOTALLEANRTASS = TOTALLEANRTASS + nowLearnAss
-        MAXJUMPBACK = max(MAXJUMPBACK, nowJumpBack)
-        TOTALJUMPBACK = TOTALJUMPBACK + nowJumpBack
-        MAXLEANRTSZ = max(MAXLEANRTSZ, nowLearntSz)
-        TOTALLEANRTSZ = TOTALLEANRTSZ + nowLearntSz
+
+
+        # Read statistic
+        nowStat = str(proc.stderr.read()).split('\\n')
+
+        # Check init
+        if len(statistic)==0:
+            title = [''] * len(nowStat)
+            statistic = [[0.0, 0.0] for _ in range(len(nowStat))]
+            for i in range(1, len(nowStat)-2):
+                title[i] = nowStat[i].split(':')[0]
+
+        # Parse statistic
+        for i in range(1, len(nowStat)-2):
+            nowVal = float(re.findall('[-+]?\d*\.\d+|[-+]?\d+', nowStat[i])[0])
+            statistic[i][0] += nowVal
+            statistic[i][1] = max(nowVal, statistic[i][1])
 
     print('SAT', SAT, '/ UNSAT', UNSAT, '/ ERROR', ERROR)
-    print('\nTime for SAT Solver')
-    print('   avg %.3f sec' % (TOTALTIME / (SAT + UNSAT)))
-    print('   max %.3f sec' % (MAXTIME))
-    print('\nBacktrack Num')
-    print('   avg', int(TOTALBACKTRACKNUM/(SAT + UNSAT)))
-    print('   max', MAXBACKTRACKNUM)
-    print('\nMax Depth')
-    print('   avg', int(TOTALDEPTH/(SAT + UNSAT)))
-    print('   max', MAXDEPTH)
-    print('\nLearnt clause')
-    print('   avg', TOTALLEANRTCLS/(SAT + UNSAT))
-    print('   max', MAXLEANRTCLS)
-    print('\nLearnt assignment')
-    print('   avg', TOTALLEANRTASS/(SAT + UNSAT))
-    print('   max', MAXLEANRTASS)
-    print('\nMax jump back')
-    print('   avg', int(TOTALJUMPBACK/(SAT + UNSAT)))
-    print('   max', MAXJUMPBACK)
-    print('\nMax learnt sz')
-    print('   avg', int(TOTALLEANRTSZ/(SAT + UNSAT)))
-    print('   max', MAXLEANRTSZ)
+    for i in range(1, len(statistic)-2):
+        print("%s : %10.2f / %10.2f" % (title[i], statistic[i][0]/(SAT + UNSAT), statistic[i][1]))
+
+print("".center(50, "="))
